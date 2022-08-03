96a5e478e4d9c1799eb31b2a211477a50a0c202b
Apple <riyabulia12@gmail.com>
2022-08-03 13:02:55 +0530

    Add create, update, delete methods to class

--- script.js
+++ script.js
@@ -1,82 +1,86 @@
 class Books {
-   constructor(id, title, author) {
-      this.id = id;
-      this.title = title;
-      this.author = author;
+   constructor() {
       this.bookList = [];
    }
    static fetchBooks() {
-      return JSON.parse(localStorage.getItem('books'));
+      const getBooks = localStorage.getItem('books');
+      return getBooks ? JSON.parse(getBooks) : [];
    }
 
    static updateBooks(books) {
       localStorage.setItem('books', JSON.stringify(books));
    }
-}
-const addBookBtn = document.getElementById('addBookBtn');
-const bookTable = document.getElementById('bookTable');
 
-window.onload = () => { getBooksList() }
+   static getBooksList() {
+      if (Books.fetchBooks()) {
+         this.bookList = Books.fetchBooks();
 
-function getBooksList() {
-   if (Books.fetchBooks()) {
+      }
+   }
+
+   addBook(book) {
       this.bookList = Books.fetchBooks();
-      this.bookList.forEach(item => {
+      this.bookList.push(book);
+      Books.updateBooks(this.bookList);
+   }
+
+   removeBook(event) {
+      const id = parseInt(event.target.id);
+      this.bookList = Books.fetchBooks();
+      this.bookList = this.bookList.filter(i => i.id !== id);
+      event.target.parentElement.remove();
+      Books.updateBooks(this.bookList);
+   }
+}
+
+window.onload = () => {
+   Books.getBooksList();
+   if (Books.bookList) {
+      Books.bookList.forEach(item => {
          createBookRow(item);
       })
    }
 }
 
-//Add Books to Table from local storage
+function createBookRow(item) {
+   const bookTable = document.getElementById('bookTable');
+   const bkRow = document.createElement("tr");
+   const bookDetail = bkRow.appendChild(document.createElement('td'));
+   const removeBtn = bkRow.appendChild(document.createElement('button'));
+
+   bkRow.setAttribute('id', item.id);
+   bookDetail.innerHTML = `"${item.title}" by ${item.author}`;
+   removeBtn.innerHTML = 'Remove';
+   removeBtn.setAttribute('class', 'removeBtn')
+   removeBtn.setAttribute('id', item.id)
+   removeBtn.classList.add('btn');
+   bookTable.appendChild(bkRow);
+
+   const removeBtns = document.querySelectorAll('.removeBtn');
+   removeBtns.forEach((elem) => {
+      elem.addEventListener('click', (event) => {
+         const bookObj = new Books();
+         bookObj.removeBook(event);
+      })
+   })
+}
+
+const addBookBtn = document.getElementById('addBookBtn');
 addBookBtn.onclick = function () {
-   this.bookList = Books.fetchBooks() ? Books.fetchBooks() : [];
+   Books.bookList = Books.fetchBooks() ? Books.fetchBooks() : [];
 
    let id = 0;
-   if (this.bookList && this.bookList.length > 0) {
-      id = this.bookList[this.bookList.length - 1].id + 1
+   if (Books.bookList && Books.bookList.length > 0) {
+      id = Books.bookList[Books.bookList.length - 1].id + 1
    }
    const title = document.getElementById('title').value;
    const author = document.getElementById('author').value;
 
    if (title && author) {
-      const book = new Books(id++, title, author);
-      this.bookList.push(book);
-
-      localStorage.setItem('books', JSON.stringify(this.bookList));
+      const book = { 'id': id++, 'title': title, 'author': author }
+      const bookObj = new Books();
+      bookObj.addBook(book);
       createBookRow(book);
    }
 }
-
-//Populate book details from store with remove button
-function createBookRow(item) {
-   const bkRow = document.createElement("tr");
-   bkRow.setAttribute('id', item.id);
-
-   const bookDetail = bkRow.appendChild(document.createElement('td'));
-   bookDetail.innerHTML = `"${item.title}" by ${item.author}`;
-
-   const removeBtn = bkRow.appendChild(document.createElement('button'));
-   removeBtn.innerHTML = 'Remove';
-   removeBtn.setAttribute('class', 'removeBtn')
-   removeBtn.setAttribute('id', item.id)
-   removeBtn.classList.add('btn');
-
-   bookTable.appendChild(bkRow);
-   removeBook();
-}
-
-function removeBook() {
-   const removeBooks = document.querySelectorAll('.removeBtn');
-
-   removeBooks.forEach((elem) => {
-      elem.addEventListener('click', (event) => {
-         const id = parseInt(event.target.id);
-         this.bookList = this.bookList.filter(item => item.id !== id);
-         Books.updateBooks(this.bookList);
-         event.target.parentElement.remove();
-         location.reload();
-      })
-   })
-}
-
 
